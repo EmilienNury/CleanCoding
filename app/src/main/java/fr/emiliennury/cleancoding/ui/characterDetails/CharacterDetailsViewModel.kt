ce431c14f6bbd904623a63c24851b78f22a8797b
Emilien <emilien.nury@etu.univ-lyon1.fr>
2022-04-15 10:31:15 +0200

    created nav_graph and fragments

---
+++ app/src/main/java/fr/emiliennury/cleancoding/ui/characterDetails/CharacterDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package fr.emiliennury.cleancoding.ui.characterDetails
+
+import androidx.lifecycle.ViewModel
+
+class CharacterDetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/fr/emiliennury/cleancoding/ui/characterList/CharacterListViewModel.kt
@@ -0,0 +1,7 @@
+package fr.emiliennury.cleancoding.ui.characterList
+
+import androidx.lifecycle.ViewModel
+
+class CharacterListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
